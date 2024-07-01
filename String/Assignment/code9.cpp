eeb6ca505a494a618882808bb0e177da617fd2bc
Nitishrai1 <nitish.12208500@lpu.in>
2024-07-01 22:43:36 +0530

    assignment

---
+++ String/Assignment/code9.cpp
@@ -0,0 +1,33 @@
+// wap to convert a string to integer 
+#include<bits/stdc++.h>
+using namespace std;
+blic:
+    int myAtoi(string s) {
+        // int integer=atoi(s.c_str());
+        // return integer;
+       int integer=0;
+       int index=0;
+       while(index<s.length() && s[index]==' '){
+            index++;
+       }
+       int sign=1;
+       if(index<s.length() && (s[index]=='-' || s[index]=='+')){
+            sign=(s[index]=='-')?-1:1;
+            index++;
+       }
+       while(index<s.length() && isdigit(s[index])){
+            int digit=s[index]-'0';
+            if (integer > (INT_MAX - digit) / 10) {
+            return sign == -1 ? INT_MIN : INT_MAX;
+        }
+            integer=integer*10+(digit);
+            index++;
+
+       }
+        
+       return integer*sign;
+    }
+int main() {
+
+return 0;
+}
