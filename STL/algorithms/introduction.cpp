cec5f222aa1dcfe69ebcc2f6209395f9cfe58acf
Nitishrai1 <nitish.12208500@lpu.in>
2024-07-04 16:33:11 +0530

    algorithum

--- STL/algorithms/introduction.cpp
+++ STL/algorithms/introduction.cpp
@@ -29,7 +29,28 @@ int main() {
     // int cnt=count(arr.begin(),arr.end(),target);
     // cout<<target<<" occur "<<cnt<<" times";
 
-    
+    // count_if(arr.begin(),arr.end(),checkeven); this will return how many number between begin to end which is even
+
+    // sort(arr.begin(),arr.end());
+
+    // reverse(arr.begin(),arr.end());
+    for(int n:arr){
+        cout<<n<<" ";
+    }cout<<endl;
+    rotate(arr.begin(),arr.begin()+3,arr.end()); //right rotate by three element
+    for(int n:arr){
+        cout<<n<<" ";
+    }cout<<endl;
+
+    rotate(arr.begin(),arr.end()-3,arr.end()); //left shift by three position
+     for(int n:arr){
+        cout<<n<<" ";
+    }
+
+    // unique :it removes the duplicate element
+    auto it=unique(arr.begin(),arr.end()); //return the iterator to the lat posiiton after slecteing all uniwue eleement
+    arr.erase(it,arr.end()); //removing the duplicate element from the array
+
 
 
 
