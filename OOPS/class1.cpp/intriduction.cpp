d5d1fdb4a6bea910bdcf04807570bbeb81af648f
Nitishrai1 <nitish.12208500@lpu.in>
2024-07-15 21:36:46 +0530

    updatE

--- OOPS/class1.cpp/intriduction.cpp
+++ OOPS/class1.cpp/intriduction.cpp
@@ -2,15 +2,24 @@
 // why : in the proceduler programing thier was many error to overcome this error oops is introduced in the industry
 #include<bits/stdc++.h>
 using namespace std;
-class Student{
+class Student {
     public: //so that the data member and data fucntion can be accesd in the main function
     int student_id;
     string student_name;
     int age;
-    Student(int id,string name,int age ){
+    Student(){}//default constructor
+    Student(int id,string name,int age ){  //parameterise constructor
         this->student_id=id;
         this->student_name=name;
         this->age=age;  
+        //this keyward demonstrate on which object/instance wer are calling the class 
+    }
+
+    // copy construnctor
+      Student(const Student& srcObj ){  //parameterise constructor
+        this->student_id=srcObj.student_id;
+        this->student_name=srcObj.student_name;
+        this->age=srcObj.age;  
         //this keyward demonstrate on which object/instance wer are calling the class 
     }
     void eat(){
@@ -29,7 +38,13 @@ class Student{
     ~Student(){} // this is distructing the class so that the memory which are allocated can be released
 };
 int main() {
-    Student s1=new Student(1,"nitish",22); //this is instance fo the class
+    Student s1(1,"Nitish",22); //this is instance fo the class
+    s1.eat(); //by this way i can access the data meber and data function of a class
+    cout<<endl;
+    s1.play();
+
+    // now how to create a copy constructor
+    Student s2=s1;//the valu of s1 objext is copied to the s2 obect
 
 
 return 0;
