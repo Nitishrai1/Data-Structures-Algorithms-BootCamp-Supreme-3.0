627e3f09ab3183711389fe7b88d622d05fcf1fa2
Nitishrai1 <nitish.12208500@lpu.in>
2024-07-18 00:01:48 +0530

    DoublyLl

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -11,6 +11,11 @@ class Node{
         this->next=NULL;
         this->prev=NULL;
     }
+    ~Node(){
+        
+        delete prev;
+        delete next; 
+    }
 };
 void createDLL(Node* &head,int data){
     if(head==nullptr){
@@ -111,7 +116,74 @@ void insertion(Node* &head,int data,int pos){
 
 
 }
+void deletehead(Node* &head){
+    // head to delete karna hai
+    if(head==NULL){
+        return; //list is already empty
+    }
+    Node* todelete=head;
+    head=head->next;
+    if(head!=NULL){
+        head->prev=NULL;
+    }
+   
+    delete todelete;
+}
+void deletetail(Node* &head){
+    // traverse karo tail tak
+    // yek case dekho lo ki agar list me yek hi node hai tab
+    if(head==NULL)return; //koi node hai hi nahi delete karne ko
+    if(head->next==NULL){
+        delete head;
+        head=nullptr;
+        return;
+    }
+    Node* temp=head;
+    while (temp->next->next!=nullptr)
+    {
+        temp=temp->next;
+        /* code */
+    }
+    
+    // last se yek index pahle a gye hai
+    Node* todelete=temp->next;
+    temp->next=NULL;
+    todelete->prev=NULL;
+    delete todelete;
+}
+void deleteinMiddle(Node* &head,int pos){
+    if(pos==1){
+        deletehead(head);
+    }
+    else if(pos==length(head)){
+        deletetail(head);
+    }
+    else if(pos>0 && pos<length(head)){
+        Node* temp=head;
+        for(int i=1;i<pos-1;i++){
+            temp=temp->next;
+        }
+        // temp yek index pahle hai
+        Node* todelete=temp->next;
+        temp->next=todelete->next;
+        if(todelete->next!=NULL){
+            todelete->next->prev=temp;
+        }
+        todelete->next=NULL;
+        todelete->prev=NULL;
+        delete todelete;
+
+    }
+
+}
+
 void Deletion(Node* &head,int pos){
+    if(pos==1){
+        deletehead(head);
+    }
+    if(pos==length(head)){
+        deletetail(head);
+    }
 
 }
 int main() {
@@ -123,22 +195,35 @@ int main() {
         createDLL(head,data);
 
     }
-    Node* temp=head;
-    cout<<"Linked list before inserting the data: ";
-    printdoublyll(temp);
+    // Node* temp=head;
+    // cout<<"Linked list before inserting the data: ";
+    // printdoublyll(temp);
     // /length
-    int len=length(temp);
-    cout<<"The length of the linked list is: "<<len<<endl;
-    // inserting the elmenet at the head of the linked list
-    int data;cout<<"\nEnter the data to insert: ";cin>>data;
-    // insertatHead(temp,data);
-    // cout<<"\nThe linked list after inserting the data at head is : ";
-    // insertatTail(head,data);
-    int pos;cout<<"Enter the postion: ";cin>>pos;
-    insertion(temp,data,pos);
-    cout<<"Linked list after inserting  is : ";
-    printdoublyll(temp);
-
+    // int len=length(temp);
+    // cout<<"The length of the linked list is: "<<len<<endl;
+    // // inserting the elmenet at the head of the linked list
+    // int data;cout<<"\nEnter the data to insert: ";cin>>data;
+    // // insertatHead(temp,data);
+    // // cout<<"\nThe linked list after inserting the data at head is : ";
+    // // insertatTail(head,data);
+    // int pos;cout<<"Enter the postion: ";cin>>pos;
+    // insertion(temp,data,pos);
+    // cout<<"Linked list after inserting  is : ";
+    cout<<"Linked list before deleting the data: ";
+    
+    printdoublyll(head);
+    deletehead(head);
+    // deletetail(temp);
+   
+    cout<<"Printing the list afterdelete hte : ";
+    printdoublyll(head);
+    // Clean up allocated memory
+    Node* temp;
+    while (head != nullptr) {
+        temp = head->next;
+        delete head;
+        head = temp;
+    }
 
 return 0;
 }
