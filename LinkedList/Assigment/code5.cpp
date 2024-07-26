1b61bd03f63205ac50b8640dba9271a090d93245
Nitishrai1 <nitish.12208500@lpu.in>
2024-07-26 21:20:19 +0530

    assignment

--- LinkedList/Assigment/code5.cpp
+++ LinkedList/Assigment/code5.cpp
@@ -22,6 +22,64 @@ using namespace std;
 //     }
 
 // optimal
+//    ListNode* findMid(ListNode* head){
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         ListNode* prev=nullptr;
+//         while(fast && fast->next){
+//             prev=slow;
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+//         if(prev!=NULL){
+//             prev->next=nullptr;
+//         }
+//         return slow;
+//     }
+//     ListNode* merge(ListNode* list1, ListNode* list2) {
+//         if (list1 == NULL) return list2;
+//     if (list2 == NULL) return list1;
+
+//     ListNode dummy(0); //dummy node banao
+//     ListNode* curr = &dummy;
+//     while (list1 != NULL && list2 != NULL) {
+//         if (list1->val < list2->val) {
+//             curr->next = list1;
+//             list1 = list1->next;
+//         } else {
+//             curr->next = list2;
+//             list2 = list2->next;
+//         }
+//         curr = curr->next;
+//     }
+// // bacah hua jodo
+//     if (list1 != NULL) {
+//         curr->next = list1;
+//     } else {
+//         curr->next = list2;
+//     }
+
+//     return dummy.next;
+//     }
+//     ListNode* sortList(ListNode* head) {
+//         if(head==NULL || head->next==NULL){
+//             return head;
+//         }
+//         // break karo 
+//         ListNode* mid=findMid(head);
+//         ListNode* left=head;
+//         ListNode* right=mid;
+        
+    
+//     // recursive call karo jab tak single element na bache
+//         left=sortList(left);
+//         right=sortList(right);
+
+//         // merge kar do
+//         ListNode* ans= merge(left,right);
+//         return ans;
+        
+//     }
 
 int main() {
 
---
+++ LinkedList/Assigment/code6.cpp
@@ -0,0 +1,32 @@
+// flattern a ll'
+#include<bits/stdc++.h>
+using namespace std;
+// Node* merge(Node* a,Node* b){
+//         if(!a)return b;
+//         if(!b)return a;
+        
+//         Node* ans=0;
+//         if(a->data<b->data){
+//             ans=a;
+//             ans->bottom=merge(a->bottom,b);
+//         }
+//         else{
+//             ans=b;
+//             ans->bottom=merge(a,b->bottom);
+//         }
+//         return ans;
+//     }
+//     Node *flatten(Node *root)
+//     {
+//         // Your code here
+//         // sabse pahle do sublist nikalo or merge kar do
+//         // fir res se dusara sublist merge kar do
+//         if(root==NULL)return 0;
+//         Node* mergedll=merge(root,flatten(root->next));
+//         return mergedll;
+        
+//     }
+int main() {
+
+return 0;
+}
