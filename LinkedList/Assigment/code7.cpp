0b15801762304a19c57b65a56d2bd666914849d7
Nitishrai1 <nitish.12208500@lpu.in>
2024-07-27 10:24:15 +0530

    assignment

---
+++ LinkedList/Assigment/code7.cpp
@@ -0,0 +1,32 @@
+// Rotate a linked list 
+#include<bits/stdc++.h>
+using namespace std;
+//  ListNode* temp=head;
+//         if(k<=0 || !temp) return head;
+
+//         // length nikal lo
+//         int len=0;
+//         while(temp){
+//             len++;
+//             temp=temp->next;
+//         }
+//         // check kar lo k ko
+//         k=k%len;
+//         temp=head;
+//         while(k--){
+//             ListNode* prev=nullptr;
+//             while(temp->next!=NULL){
+//                 prev=temp;
+//                 temp=temp->next;
+//             }
+//             // ab prev last index se yek pahle ko point kar raha hai
+//             prev->next=NULL;
+//             temp->next=head;
+//             head=temp;
+//             // temp=head;
+//         }
+//         return head;
+int main() {
+
+return 0;
+}
---
+++ LinkedList/Assigment/code8.cpp
@@ -0,0 +1,56 @@
+// Merge nodes in between zeros
+#include<bits/stdc++.h>
+using namespace std;
+// ListNode* mergeNodes(ListNode* head) {
+//         ListNode* newnode=nullptr;
+//         ListNode* curr=NULL;
+//         int sum=0;
+//         ListNode* temp=head->next;
+//         while(temp!=NULL){
+//             if(temp->val==0){
+//                 if(newnode==NULL){
+//                     newnode=new ListNode(sum);
+//                     curr=newnode;
+//                 }else{
+//                     curr->next=new ListNode(sum);
+//                     curr=curr->next;
+                    
+                    
+//                 }
+                    
+//                 sum=0;
+                
+//             }
+//             else{
+
+//                 sum+=temp->val;
+//             }
+//             temp=temp->next;
+//         }
+//             // sum=0;
+//         //     while(temp->val!=0){
+//         //         sum+=temp->val;
+//         //         temp=temp->next;
+//         //     }
+//         //     if(newnode==NULL){
+//         //         newnode=new ListNode(sum);
+//         //         curr=newnode;
+                
+//         //     }else{
+//         //         curr->next=new ListNode(sum);
+//         //         curr=curr->next;
+                
+
+//         //     }
+//         //     temp=temp->next;
+
+//         // }
+
+//         return newnode;
+        
+        
+//     }
+int main() {
+
+return 0;
+}
