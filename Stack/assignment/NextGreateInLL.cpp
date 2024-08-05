8ec007abb03d9ef2293bc7a8ad7d903302fa8e48
Nitishrai1 <nitish.12208500@lpu.in>
2024-08-05 16:36:23 +0530

    assignment

---
+++ Stack/assignment/NextGreateInLL.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+// class Solution {
+// public:
+//     vector<int> nextLargerNodes(ListNode* head) {
+//         stack<int> st;
+//         st.push(0);
+//         vector<int> temp;
+//         ListNode* t=head;
+//         while(t){
+//             temp.push_back(t->val);
+//             t=t->next;
+//         }
+//         for(int i:temp){
+//             cout<<i<<" ";
+//         }
+//         int n=temp.size();
+//         vector<int> ans(n,0);
+
+//         for(int i=n-1;i>=0;i--){
+//             while(st.top()!=0 && st.top()<=temp[i]){
+//                 st.pop();
+//             }
+
+//             if(st.top()!=0){
+//                 ans[i]=st.top();
+//             }
+//             st.push(temp[i]);   //ye line me bhul jata hu
+//         }
+//         return ans;
+        
+//     }
+// };
+int main() {
+
+return 0;
+}
