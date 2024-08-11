34cddf941553c8ca7a3bc9a477c867c3e1d0c009
Nitishrai1 <nitish.12208500@lpu.in>
2024-08-11 23:27:29 +0530

    up

--- Stack/assignment/carfleet.cpp
+++ Stack/assignment/carfleet.cpp
@@ -39,8 +39,9 @@ int carFleet(int target, vector<int>& position, vector<int>& speed) {
         }
         // jo size hai usme se 2*cnt minus karke add kar do
         return st.size();
+        //
         
-    }
+    }  
 int main() {
 
 return 0;
