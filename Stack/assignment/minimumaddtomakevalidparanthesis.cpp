e2960d57a477251c40ec69a2fe38a0026b9e50ec
Nitishrai1 <nitish.12208500@lpu.in>
2024-08-13 10:25:30 +0530

    assignment

---
+++ Stack/assignment/minimumaddtomakevalidparanthesis.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==')'){
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                }else{
+                    st.push(s[i]);
+                }
+            }
+            else st.push(s[i]);
+        }
+        if(st.empty()){
+            return 0;
+        }
+        return st.size();
+        
+    }
+int main() {
+
+return 0;
+}
