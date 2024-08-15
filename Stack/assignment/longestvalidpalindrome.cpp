d74b7ad60abac0363c1537ef8b6490ba9cf696ce
Nitishrai1 <nitish.12208500@lpu.in>
2024-08-15 23:23:39 +0530

    assignment

---
+++ Stack/assignment/longestvalidpalindrome.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int longestValidParentheses(string s) {
+
+        stack<int> st;
+        st.push(-1);
+        int maxi=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            if(ch=='('){
+                st.push(i);
+            }else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }else{
+                    int len=i-st.top();
+                    maxi=max(maxi,len);
+                }
+            }
+        }
+        return maxi;
+int main() {
+
+return 0;
+}
